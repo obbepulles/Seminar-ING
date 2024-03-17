f8abf62a88af26655e19a7f20f18897927c053c7
Obbe <obbe.pulles@gmail.com>
2024-03-17 20:19:58 +0100

    FINAL

--- obbe_FINAL.R
+++ obbe_FINAL.R
@@ -22,7 +22,6 @@ rate_data <- readxl::read_xlsx("hypothetical_data_set.xlsx",1, skip = 1, range =
 ftp_data <- readxl::read_xlsx("hypothetical_data_set.xlsx",1, skip = 1, range = "A2:F122")
 ftp_2y <- as.vector(unname(ftp_data[, 2]))[[1]]
 
-
 data <- data %>% mutate(month_year = format(`Reporting date`, "%m-%Y")) 
 rate_data <- rate_data %>% mutate(month_year = format(`Date`, "%m-%Y")) 
 ftp_data <- ftp_data %>% mutate(month_year = format(`Date`, "%m-%Y"))
@@ -72,10 +71,42 @@ error <- modeldata$Y - modeldata$X * tobit_model$estimate[2] - tobit_model$estim
 # Sys.sleep(4)
 # }
 
+#--- Autocorrealtion part of the analysis of utilisation
+#Filter out constant U_t's
+filtered_df <- data %>%
+  group_by(Client) %>%
+  filter((length(unique(`Used amount`)) > 1) & (length(`Used amount`) > 23)) %>%
+  ungroup()
+
+lagged_use_filtered <- filtered_df %>% 
+  group_by(Client) %>%
+  mutate(dU = `Used amount` - lag(`Used amount`))
+
+#Autocorrelation U_t 
+autoCorr <- filtered_df %>% group_by(Client) %>% 
+  summarise(Autocorrelation = acf(`Used amount`, plot = FALSE)$acf[2])
+summary(autoCorr[2])
+hist(autoCorr$Autocorrelation)
+
+#Autocorrelation dU_t
+lagged_use_filtered <- lagged_use_filtered %>% filter(!is.na(dU))
+autoCorrdU <- lagged_use_filtered %>% group_by(Client) %>% summarise(Autocorrelation = acf(dU, plot = FALSE)$acf[2])
+summary(autoCorrdU[2])
+hist(autoCorrdU$Autocorrelation)
+qqnorm(autoCorrdU$Autocorrelation)
+qqline(autoCorrdU$Autocorrelation)
+
+pcaf_pvals <- rep(0,10)
+for(i in 1:10){
+  pacf_results <- filtered_df %>%
+    group_by(Client) %>%
+    summarize(PACF = list(pacf(`Used amount`, plot = FALSE)$acf[i]))
+  corls <- as.numeric(pacf_results$PACF)
+  pcaf_pvals[i] <- t.test(x = corls, alternative = "two.sided")$p.value
+}
 
 ######################################################################################################
-#--- Most up-to-date data analysis starts
-#Filter those clients whose contracts have already matured
+#---Filter those clients whose contracts have already matured
 #Last reporting date is 31-12-2021
 
 #Clients whose contracts are either finished or cancelled are included
